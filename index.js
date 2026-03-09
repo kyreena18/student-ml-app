42dd94a66daa56b772f7b84cdfb296be85e95a8a
Kyreena Daruwalla <157062399+kyreena18@users.noreply.github.com>
2026-03-09 19:14:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App(){
+    return(
+        <div>
+            <h1>Hello</h1>
+            <p>this is my first react app</p>
+        </div>
+    )
+}
+
+export default App;
