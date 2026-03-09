a7bcf006c2f06288e287b4a678402dc478cd0f64
Kyreena Daruwalla <157062399+kyreena18@users.noreply.github.com>
2026-03-09 19:20:27 +0530

    Update App component with new greeting and message

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import React from "react";
 
-function App(){
-    return(
-        <div>
-            <h1>Hello</h1>
-            <p>this is my first react app</p>
-        </div>
-    );
+function App() {
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <p>Welcome to my first React application.</p>
+    </div>
+  );
 }
-
 
 export default App;
